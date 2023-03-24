665dc70ddadb8ba0ebff2bc900c10729aefaa13c
DannyDannyDanny <dnth@lundbeck.com>
2023-03-24 11:33:09 +0100

    :art: run `black --line-length 150`

--- scripts/submit.py
+++ scripts/submit.py
@@ -1,7 +1,5 @@
-
-
 def submit_dashboard(name, port, model, X_test, y_test):
-    dashboard_name = 'yeet'
+    dashboard_name = "yeet"
     explainer = RegressionExplainer(model, X_test, y_test)
 
     DIRECTORY_TO_WATCH = "/home/sagemaker-user/dashboard-definitions"
@@ -9,16 +7,13 @@ def submit_dashboard(name, port, model, X_test, y_test):
         explainer,
         title=name,
         whatif=False,
-        mode='dash',
+        mode="dash",
         port=port,
-        routes_pathname_prefix='/',
-        requests_pathname_prefix=f'/jupyter/default/proxy/{PORT}/'
+        routes_pathname_prefix="/",
+        requests_pathname_prefix=f"/jupyter/default/proxy/{PORT}/",
     )
 
-    filename = name.replace(' ', '_').replace('.', '_')
+    filename = name.replace(" ", "_").replace(".", "_")
 
-    db.to_yaml(
-        filepath=f"{DIRECTORY_TO_WATCH}/{filename}.yaml",
-        explainerfile=f"{DIRECTORY_TO_WATCH}/{filename}.joblib",
-        dump_explainer=True)
-    print(f'<STUDIO_URL>/jupyter/default/proxy/{port}')
+    db.to_yaml(filepath=f"{DIRECTORY_TO_WATCH}/{filename}.yaml", explainerfile=f"{DIRECTORY_TO_WATCH}/{filename}.joblib", dump_explainer=True)
+    print(f"<STUDIO_URL>/jupyter/default/proxy/{port}")
